dae5f12467d5fb5461c5fb14978ce1d3639f45d4
v2verma1990 <vishal.is.verma@gmail.com>
2022-09-23 23:26:10 +0100

    first commit

---
+++ filter.py
@@ -0,0 +1,16 @@
+import sys
+import logging
+def slash_access(obj, keys):
+    keys = keys.split("/")
+    current = obj
+    for key in keys:
+        current = current[key]
+    return current
+
+my_object = {"a":{"b":{"c":"d"}}}
+
+try:
+    result =slash_access(my_object, "a/b")
+    print(result)
+except BaseException:
+    logging.exception("Incorrect key")
---
+++ filter_test.py
@@ -0,0 +1,18 @@
+import unittest
+from filter import slash_access
+
+class TestFactorial(unittest.TestCase):
+    def test_fact(self):
+        
+        my_object = {"a":{"b":{"c":"d"}}}
+        res =  slash_access(my_object,"a/b")
+        self.assertEqual(res, {'c': 'd'})
+      
+    def test_error(self):
+        my_object = {"a":{"b":{"c":"d"}}}
+        with self.assertRaises(BaseException):
+           slash_access(my_object,"a/c")
+       
+
+if __name__ == '__main__':
+    unittest.main()
